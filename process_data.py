c00638bf2298cea1ff0c3ce8a22d53f7ca6a266d
jbenn88 <jamesa.benn@gmail.com>
2022-05-02 12:12:27 -0400

    updated and simplified data structures

--- process_data.py
+++ process_data.py
@@ -1,11 +1,7 @@
 import os
 
 dates = []
-line_jobs = []
-total_jobs = []
-times = [ ['Sunday', []], ['Monday', []], ['Tuesday', []], ['Wednesday',[]], ['Thusday', []], ['Friday', []], ['Saturday', []] ]
-
-
+daily_data = [ ['Sunday', [], [], []], ['Monday', [], [], []], ['Tuesday', [], [], []], ['Wednesday', [], [], []], ['Thusday', [], [], []], ['Friday', [], [], []], ['Saturday', [], [], []] ]
 
 f = open('Rev_Job_Trends/rev_jobs.txt', 'r')
 
@@ -14,21 +10,36 @@ for row in f:
     day = row[0] # First value from line of text file
     match day:
         case 'Sunday':
-            times[0][1].append(row[2] + " " + row[3])
+            daily_data[0][1].append(row[2] + " " + row[3])
+            daily_data[0][2].append(row[5].rstrip(os.linesep).rstrip('\t#'))
+            daily_data[0][3].append(row[4])
         case 'Monday':
-            times[1][1].append(row[2] + " " + row[3])
+            daily_data[1][1].append(row[2] + " " + row[3])
+            daily_data[1][2].append(row[5].rstrip(os.linesep).rstrip('\t#'))
+            daily_data[1][3].append(row[4])
         case 'Tuesday':
-            times[2][1].append(row[2] + " " + row[3])
+            daily_data[2][1].append(row[2] + " " + row[3])
+            daily_data[2][2].append(row[5].rstrip(os.linesep).rstrip('\t#'))
+            daily_data[2][3].append(row[4])
         case 'Wednesday':
-            times[3][1].append(row[2] + " " + row[3])
+            daily_data[3][1].append(row[2] + " " + row[3])
+            daily_data[3][2].append(row[5].rstrip(os.linesep).rstrip('\t#'))
+            daily_data[3][3].append(row[4])
         case 'Thursday':
-            times[4][1].append(row[2] + " " + row[3])
+            daily_data[4][1].append(row[2] + " " + row[3])
+            daily_datas[4][2].append(row[5].rstrip(os.linesep).rstrip('\t#'))
+            daily_data[4][3].append(row[4])
         case 'Friday':
-            times[5][1].append(row[2] + " " + row[3])
+            daily_data[5][1].append(row[2] + " " + row[3])
+            daily_data[5][2].append(row[5].rstrip(os.linesep).rstrip('\t#'))
+            daily_data[5][3].append(row[4])
         case 'Saturday':
-            times[6][1].append(row[2] + " " + row[3])
+            daily_data[6][1].append(row[2] + " " + row[3])
+            daily_data[6][2].append(row[5].rstrip(os.linesep).rstrip('\t#'))
+            daily_data[6][3].append(row[4])
     dates.append(row[1])
-    line_jobs.append(int(row[5].rstrip(os.linesep).rstrip('\t#')))
 
-for day in times:
+#for day in line_jobs:
+#    print(day)
+for day in daily_data:
     print(day)
