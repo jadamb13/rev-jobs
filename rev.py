8371f4620a6446d410074161808a94280dcbfdb9
jbenn88 <jamesa.benn@gmail.com>
2022-05-07 00:22:21 -0400

    Cleaning up

--- rev.py
+++ rev.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
-from bs4 import BeautifulSoup
 import lxml
 from datetime import date, time, datetime
 import io
@@ -21,9 +20,6 @@ driver = webdriver.Firefox(options=options, executable_path="C:\\Users\\james\\D
 driver.get(url)
 
 # Log in #
-#login_button = '/html/body/div/nav/div/div/a[1]/span'
-#driver.find_element_by_xpath(login_button).click()
-
 username_xpath = '//*[@id="Email"]'
 driver.find_element_by_xpath(username_xpath).send_keys(username)
 
@@ -38,10 +34,6 @@ WebDriverWait(driver, 1000000).until(EC.element_to_be_clickable((By.XPATH, passw
 sign_in_xpath = '//*[@id="login-btn"]'
 driver.find_element_by_xpath(sign_in_xpath).click()
 
-# Get amount of jobs/line jobs #
-
-job_page_soup = BeautifulSoup(driver.page_source, 'lxml')
-
 time.sleep(10) # Will not find element without waiting
 number_of_jobs_xpath = '/html/body/div[2]/div/div[2]/div/div/div/div/div[2]/div/div[1]/div[1]/div/span[1]/a[1]/span[2]'
 number_of_jobs = driver.find_element_by_xpath(number_of_jobs_xpath).text
@@ -51,20 +43,14 @@ number_of_line_jobs_xpath = '/html/body/div[2]/div/div[2]/div/div/div/div/div[2]
 number_of_line_jobs = driver.find_element_by_xpath(number_of_line_jobs_xpath).text
 number_of_line_jobs = number_of_line_jobs.replace("(", "").replace(")", "")
 
-# Click line jobs to get more information specifically about line jobs available #
-
-#line_jobs_button = '/html/body/div[2]/div/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div[1]/div/span[1]/a[3]'
-#driver.find_element_by_xpath(line_jobs_button).click()
-
 # Save information to file/spreadsheet #
 time_and_date = datetime.now()
 string_time_and_date = time_and_date.strftime("%A %m/%d/%Y %I:%M %p")
 file_name = "C:\\Users\\james\\Desktop\\Rev_Job_Trends\\rev_jobs.txt"
+
 with open(file_name, "a+") as source_file:
     source_file.write(string_time_and_date + " " + number_of_jobs + " " + number_of_line_jobs + "\n")
 
 driver.close()
 
-# TODO: create new text files for each month to keep data separated for graphing
-# TODO: add line jobs to file as well as overall jobs and plot them
-# TODO: refactor/replaced deprecated commands
+# TODO: refactor/replaced deprecated commands for Selenium/BS4
