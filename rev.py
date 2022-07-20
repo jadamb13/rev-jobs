ab5339ad91e88584b97a2ecaa880f3b2af4308eb
jbenn88 <jamesa.benn@gmail.com>
2022-07-20 02:21:25 -0400

    cleans up toast calls and adds percentage of jobs with under 2  "unclaims" to notification

--- rev.py
+++ rev.py
@@ -114,16 +114,14 @@ with open(file_name, "a+") as source_file:
 # Sends first notification with number of total jobs and line jobs; customized to send if greater than X number of total jobs
 toast = ToastNotifier()
 
-
 if (int(number_of_jobs) > 10):
     toast.show_toast("Rev Jobs Available", "There are " + number_of_jobs + " total jobs, and " + number_of_line_jobs + " line jobs currently available.")
-    toast.show_toast("Jobs with 0 unclaims: " + str(zero_unclaim_count) + " | Jobs with 1-2 unclaims: " + str(number_of_jobs_with_one_or_two_unclaims) + " | Percentage under two unclaims: " + str(percentage_of_jobs_with_under_two_unclaims))
+    toast.show_toast("Jobs with 0 unclaims: " + str(zero_unclaim_count) + "/" + str(len(unclaims)) + " | Jobs with 1-2 unclaims: " + str(number_of_jobs_with_one_or_two_unclaims) + "/" + str(len(unclaims)) +"(" + str(percentage_of_jobs_with_under_two_unclaims * 100) + "%)")
+
+
 # Send second notification with number of jobs under 5 and 10 minutes
 if (under_ten_count > 5 or under_five_count > 2):
     toast.show_toast("There are " + str(under_ten_count) + " jobs under 10 minutes and " + str(under_five_count) + " jobs under 5 minutes.")
-
-
-
 
 # TODO: Add number of jobs below X minutes in length; number of video jobs; number of audio jobs; number and percentage of jobs with 0 - X unclaims
 # TODO: Write documentation and set up to be portable/usable by other rev members
