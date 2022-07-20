402bb8cda64c3c01be990a660ce13bce04bbe08d
jbenn88 <jamesa.benn@gmail.com>
2022-07-20 02:09:40 -0400

    adds notification that includes number of jobs with 0 "unclaims" and jobs with 2 or less "unclaims"

--- rev.py
+++ rev.py
@@ -78,14 +78,20 @@ video_jobs = len(all_video_divs)
 unclaim_divs = driver.find_elements(By.XPATH, "//span[@class='unclaim-count']")
 unclaims = []
 zero_unclaim_count = 0
+one_unclaim_count = 0
+two_unclaim_count = 0
 for div in unclaim_divs:
     unclaims.append(int(div.text))
 for unclaim in unclaims:
     if (unclaim == 0):
         zero_unclaim_count += 1
+    if (unclaim == 1):
+        one_unclaim_count += 1
+    if (unclaim == 2):
+        two_unclaim_count += 1
 
-number_of_jobs_with_one_or_two_unclaims = 0
-percentage_of_jobs_with_under_two_unclaims = 0
+number_of_jobs_with_one_or_two_unclaims = one_unclaim_count + two_unclaim_count
+percentage_of_jobs_with_under_two_unclaims = (number_of_jobs_with_one_or_two_unclaims / len(unclaims))
 
 driver.close()
 
@@ -107,14 +113,17 @@ with open(file_name, "a+") as source_file:
 
 # Sends first notification with number of total jobs and line jobs; customized to send if greater than X number of total jobs
 toast = ToastNotifier()
-toast2 = ToastNotifier()
 
-if (int(number_of_jobs) > 45):
+
+if (int(number_of_jobs) > 10):
     toast.show_toast("Rev Jobs Available", "There are " + number_of_jobs + " total jobs, and " + number_of_line_jobs + " line jobs currently available.")
-
+    toast.show_toast("Jobs with 0 unclaims: " + str(zero_unclaim_count) + " | Jobs with 1-2 unclaims: " + str(number_of_jobs_with_one_or_two_unclaims) + " | Percentage under two unclaims: " + str(percentage_of_jobs_with_under_two_unclaims))
 # Send second notification with number of jobs under 5 and 10 minutes
 if (under_ten_count > 5 or under_five_count > 2):
-    toast2.show_toast("There are " + str(under_ten_count) + " jobs under 10 minutes and " + str(under_five_count) + " jobs under 5 minutes.")
+    toast.show_toast("There are " + str(under_ten_count) + " jobs under 10 minutes and " + str(under_five_count) + " jobs under 5 minutes.")
+
+
+
 
 # TODO: Add number of jobs below X minutes in length; number of video jobs; number of audio jobs; number and percentage of jobs with 0 - X unclaims
 # TODO: Write documentation and set up to be portable/usable by other rev members
