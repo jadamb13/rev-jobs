85c74b3b5f288e0ceecd6cdb4e74d685a1e7d577
James Bennett <jamesa.benn@gmail.com>
2024-08-11 15:36:26 -0400

    Added random_sleep() to auth.py to replace sleep() for more flexibility and cleaner code

--- src/auth.py
+++ src/auth.py
@@ -7,25 +7,29 @@ from time import sleep
 import random
 
 
+def random_sleep(min_time=1, max_time=5):
+    sleep(random.randint(min_time, max_time))
+
+
 def login(driver):
 
     config = get_config()
 
     # Log in #
     driver.get(config['url'])
-    sleep(random.randint(1, 5))
+    random_sleep()
     driver.find_element(By.XPATH, config['login_button_xpath']).click()
     driver.find_element(By.XPATH, config['username_xpath']).send_keys(config['username'])
-    sleep(random.randint(1, 5))
+    random_sleep()
     driver.find_element(By.XPATH, config['next_button_xpath']).click()
 
     # WebDriverWait line found at: https://stackoverflow.com/questions/56085152/selenium-python-error-element-could
     # -not-be-scrolled-into-view to solve issue of element not being scrolled into view
-    WebDriverWait(driver, random.randint(950000, 1000000)).until(
+    WebDriverWait(driver, 30).until(
         ec.element_to_be_clickable((By.XPATH, config['password_xpath']))).send_keys(config['password'])
-    sleep(random.randint(1, 5))
+    random_sleep()
     driver.find_element(By.XPATH, config['sign_in_xpath']).click()
-    sleep(random.randint(30, 45))
+    random_sleep(20, 35)
 
     # Check for 2FA div
     try:
@@ -34,7 +38,7 @@ def login(driver):
         print("No two-factor authentication div found.")
 
     # Wait for notification pop-up
-    sleep(random.randint(10, 14))  # Will not find element without waiting
+    random_sleep(10, 14)  # Will not find element without waiting
 
     # Click "No, thanks" on notification pop up (if showing)
     try:
