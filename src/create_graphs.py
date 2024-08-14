3d35c720a14898f2fbfb7f3f51e00e40900041e0
James Bennett <jamesa.benn@gmail.com>
2024-08-13 20:10:02 -0400

    Updated data structures used to stored daily data and optimized code from quadratic time to linear time in data_processing.py

--- src/create_graphs.py
+++ src/create_graphs.py
@@ -25,7 +25,7 @@ plt.grid(True)
 
 # Creates local directories if needed and saves graph image
 figure_filepath = os.path.join(os.getcwd(), '../output')
-file_name = 'job_trends.png'
+file_name = 'maximum_jobs_daily.png'
 full_filepath = figure_filepath + '/' + file_name
 if not os.path.exists(figure_filepath):
     os.makedirs(figure_filepath)
@@ -35,3 +35,36 @@ plt.savefig(full_filepath)
 # plt.show()
 
 
+# Initialize day indices and job lists
+day_indices = []
+alj_combined = []
+atj_combined = []
+
+# Populate the lists
+for day_index, (day_name, day_data) in enumerate(daily_data.items()):
+    num_entries = len(day_data['line_jobs'])  # Number of entries for the day
+    day_indices.extend([day_index] * num_entries)  # Extend day_indices with the day_index
+    alj_combined.extend(day_data['line_jobs'])  # Extend alj_combined with line_jobs
+    atj_combined.extend(day_data['total_jobs'])  # Extend atj_combined with total_jobs
+
+# Check if day_indices has the same length as alj_combined and atj_combined
+assert len(day_indices) == len(alj_combined) == len(atj_combined), "Mismatch in list lengths!"
+
+# Create the scatterplot
+plt.figure(figsize=(10, 6))
+
+# Scatterplot for line jobs
+plt.scatter(day_indices, alj_combined, color='blue', label='Line Jobs Available')
+
+# Scatterplot for total jobs
+plt.scatter(day_indices, atj_combined, color='red', label='Total Jobs Available')
+
+# Customize the plot
+plt.title('Job Availability Throughout the Week')
+plt.xlabel('Day of the Week')
+plt.ylabel('Number of Jobs Available')
+plt.xticks(ticks=range(7), labels=days)  # Label x-axis with day names
+plt.legend()
+
+# Show the plot
+plt.show()
--- src/data_processing.py
+++ src/data_processing.py
@@ -1,24 +1,72 @@
 import os
 
+# Initialize daily data structure
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+daily_data = {day: {'times': [], 'line_jobs': [], 'total_jobs': [], 'line_max': 0, 'total_max': 0, 'line_max_time': ''}
+              for day in days}
+
+
+def update_daily_data(day, time, total_jobs, line_jobs):
+    daily_data[day]['times'].append(time)
+    daily_data[day]['line_jobs'].append(line_jobs)
+    daily_data[day]['total_jobs'].append(total_jobs)
+    daily_data[day]['line_max'] = max(daily_data[day]['line_jobs'])
+    daily_data[day]['total_max'] = max(daily_data[day]['total_jobs'])
+
+
+f = open(os.path.join(os.getcwd(), '../output/job_data.txt'), 'r')
 dates = []
+
+for row in f:
+    row = row.split()
+    dates.append(row[1])
+    day, time, total_jobs, line_jobs = row[0], f"{row[2]} {row[3]}", int(row[4]), int(
+        row[5].rstrip(os.linesep).rstrip('\t#'))
+
+    update_daily_data(day, time, total_jobs, line_jobs)
+
+f.close()
+
+# Gets unique dates and max job data for create_graphs.py
+unique_dates = set(dates)
 max_total_jobs_per_day = []
 max_line_jobs_per_day = []
-alj_combined = []
-atj_combined = []
+#alj_combined = []
+#atj_combined = []
 
-### [ [Day of the week, [Time of day], [Line Jobs Available], [Total Jobs Available],
-###   Line Jobs Daily Max, Total Jobs Daily Max, Time of Line Job Daily Max ]
-daily_data = [ ['Monday', [], [0], [0], 0, 0, ''], ['Tuesday', [], [0], [0], 0, 0, ''], ['Wednesday', [], [0], [0], 0, 0, ''],
-             ['Thursday', [], [0], [0], 0, 0, ''], ['Friday', [], [0], [0], 0, 0, ''], ['Saturday', [], [0], [0], 0, 0, ''],
-             ['Sunday', [], [0], [0], 0, 0, ''] ]
+for day_data in daily_data.values():
+    max_total_jobs_per_day.append(day_data['total_max'])
+    max_line_jobs_per_day.append(day_data['line_max'])
+
+    #alj_combined.extend(job for job in day_data['line_jobs'] if job > 0)
+    #atj_combined.extend(job for job in day_data['total_jobs'] if job > 0)
+
+# Remove leading placeholder '0' from line and total jobs lists
+for day_data in daily_data.values():
+    if len(day_data['line_jobs']) > 1:
+        day_data['line_jobs'].pop(0)
+    if len(day_data['total_jobs']) > 1:
+        day_data['total_jobs'].pop(0)
+
+'''
+# [ [Day of the week, [Time of day], [Line Jobs Available], [Total Jobs Available], Line Jobs Daily Max,
+#   Total Jobs Daily Max, Time of Line Job Daily Max ]
+daily_data = [['Monday', [], [0], [0], 0, 0, ''], ['Tuesday', [], [0], [0], 0, 0, ''],
+              ['Wednesday', [], [0], [0], 0, 0, ''],
+              ['Thursday', [], [0], [0], 0, 0, ''], ['Friday', [], [0], [0], 0, 0, ''],
+              ['Saturday', [], [0], [0], 0, 0, ''],
+              ['Sunday', [], [0], [0], 0, 0, '']]
 
 f = open(os.path.join(os.getcwd(), '../output/job_data.txt'), 'r')
 
+dates = []
+
+# Populate daily_data with job information from job_data.txt file
 for row in f:
 
     row = row.split(' ')
     dates.append(row[1])
-    day = row[0] # First value from line of text file
+    day = row[0]  # First value from line of text file
     match day:
         case 'Monday':
             daily_data[0][4] = max(daily_data[0][2])
@@ -70,8 +118,12 @@ for row in f:
             daily_data[6][2].append(int(row[5].rstrip(os.linesep).rstrip('\t#')))
             daily_data[6][3].append(int(row[4]))
 
-### Gets unique dates and max job data for create_graphs.py
+# Gets unique dates and max job data for create_graphs.py
 unique_dates = set(dates)
+max_total_jobs_per_day = []
+max_line_jobs_per_day = []
+alj_combined = []
+atj_combined = []
 
 for day in daily_data:
     max_total_jobs_per_day.append(day[5])
@@ -85,13 +137,11 @@ for day in daily_data:
         if job > 0:
             atj_combined.append(job)
 
-
 # Remove leading placeholder '0' from line and total jobs lists (throws off index
 #   that is necessary for create_graphs.py)
 for day in daily_data:
-    if (len(day[2]) > 1):
+    if len(day[2]) > 1:
         day[2].pop(0)
-    if(len(day[3]) > 1):
+    if len(day[3]) > 1:
         day[3].pop(0)
-
-
+'''
