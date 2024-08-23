11ccf760c76efabadcd189388b023f6475a815ca
James Bennett <jamesa.benn@gmail.com>
2024-08-23 01:14:06 -0400

    Added reports directory (untracked) with current and historical subdirectories and moved visualizations from output to reports

--- src/output.py
+++ src/output.py
@@ -1,5 +1,7 @@
 from config.config import get_config
 from datetime import datetime
+import os
+import shutil
 
 
 def save_data_to_file(data):
@@ -10,7 +12,25 @@ def save_data_to_file(data):
     with open(config['data_file_path'], "a+") as source_file:
         source_file.write(f"{time_and_date} {data['number_of_jobs']} {data['number_of_line_jobs']}\n")
 
-# def save_weekly_job_data():
+
+def save_weekly_job_data():
+    config = get_config()
+
+    date = datetime.now()
+    today = date.today()
+    date_today = today.strftime("%b-%d-%Y")
+    current_month = date.strftime("%B")
+    current_year = date.strftime("%Y")
+
+    # Copy current rev.py information to file in last week's job data folder
+    current_weekly_data_file = config['data_file_path']
+    weekly_data_destination_filepath = os.path.join(os.getcwd(), 'Rev_Job_Trends', str(current_year),
+                                                    str(current_month), str(date_today), 'all_job_data.txt')
+
+    with open(weekly_data_destination_filepath, 'w') as destination_file:
+        shutil.copyfile(current_weekly_data_file, weekly_data_destination_filepath)
+
+
 # def save_weekly_max_jobs_graph():
 
 '''
