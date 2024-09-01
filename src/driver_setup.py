a5897f30b1871b790c01699aae45cceb82716aff
James Bennett <jamesa.benn@gmail.com>
2024-09-01 11:09:40 -0400

    Updated firefox binary path -- set as config key/value

--- src/driver_setup.py
+++ src/driver_setup.py
@@ -8,10 +8,9 @@ def setup_driver():
     config = get_config()
     options = Options()
     options.add_argument("--headless")
-    firefox_binary_path = config['firefox_binary_path']
     #service = Service(config['gecko_driver_path'])
     options.set_preference("profile", str(config['firefox_profile_path']))
-    options.binary_location = firefox_binary_path
+    options.binary_location = config['firefox_binary_path']
     driver = webdriver.Firefox(options=options)
     return driver
 
